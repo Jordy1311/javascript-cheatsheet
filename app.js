38d0ec28e2ec7bc039fc3a14d737875d620a92c0
Jordy1311 <jordancooper1195@gmail.com>
2021-03-10 13:19:24 +1300

    Numbers and the math object

--- app.js
+++ app.js
@@ -169,3 +169,41 @@ const sum = Number(val1 + val2); // without "Number" it will pass a string
 console.log(sum);
 console.log(typeof sum);
 */
+
+
+/*
+// NUMBERS AND THE MATH OBJECT
+ const num1 = 100;
+ const num2 = 50;
+ let val;
+
+ // Simple math with numbers
+ val = num1 + num2;
+ val = num1 * num2;
+ val = num1 - num2;
+ val = num1 / num2;
+ val = num1 % num2;
+
+ // Math Object
+ val = Math.PI;
+ val = Math.E;
+
+ val = Math.round(2.4);
+ val = Math.ceil(2.3);
+ val = Math.floor(2.8);
+ val = Math.sqrt(64);
+ val = Math.abs(-3);
+ val = Math.pow(8,2);
+
+ val = Math.min(2,33,4,1,55,6,3,-2);
+ val = Math.max(2,33,4,1,55,6,3,-2);
+
+ val = Math.random();
+ val = Math.random() * 20 + 1; 
+ val = Math.floor(Math.random() * 20 +1);
+
+ console.log(val);
+ */
+
+
+ 
