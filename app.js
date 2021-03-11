5c40b0062155a30fb952cca62dcb1aaadd37f71d
Jordy1311 <jordancooper1195@gmail.com>
2021-03-11 14:10:05 +1300

    Template Literals & Arrays & Array Methods

--- app.js
+++ app.js
@@ -88,6 +88,7 @@
 */
 
 
+/*
 //  PRIMITIVE
 // String (quation marks)
     const name = 'John Doe';
@@ -117,4 +118,106 @@
 
 console.log(today);
 console.log(typeof today);
+*/
 
+
+/*
+// TEMPLATE LITERALS
+    const name = 'John';
+    const age = 31;
+    const job = 'Web Developer';
+    const city = 'Miami';
+    let html;
+
+    // Without template strings (es5)
+    html = '<ul><li>Name: ' + name + ' </li><li>Age: ' + age + ' </li><li>Job: ' + job + ' </li><li>City: ' + city + ' </li></ul>';
+
+    html = '<ul>' +
+            '<li>Name: ' + name + ' </li>' +
+            '<li>Age: ' + age + ' </li>' +
+            '<li>Job: ' + job + ' </li>' +
+            '<li>City: ' + city + ' </li>'+
+            '</ul>';
+
+    function hello(){
+        return 'hello';
+    }
+
+    // With template strings (es6)
+    html = `
+        <ul>
+            <li>Name: ${name}</li>
+            <li>Age: ${age}</li>
+            <li>Job: ${job}</li>
+            <li>City: ${city}</li>
+            <li>${2 + 2}</li>
+            <li>${hello()}</li>
+            <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+        </ul>
+    `;
+
+    document.body.innerHTML = html;
+*/
+
+
+/*
+// ARRAYS AND ARRAY METHODS
+    // Create some arrays
+    const numbers = [43,56,33,23,44,36,5];
+    const numbers2 = new Array(22,45,33,76,54);
+    const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+    const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
+
+    let val;
+
+    // Get array length
+    val = numbers.length;
+    // Check if is array
+    val = Array.isArray(numbers); //'Numbers' const is array, put a string in this field, would show false
+    // Get a single value
+    val = numbers[3];
+    val = numbers[0];
+    // Insert into array
+    numbers[2] = 100;
+    // Find index of value
+    val = numbers.indexOf(36);
+
+    // // MUTATING ARRAYS
+    // // Add on to end
+    // numbers.push(250);
+    // // Add on to front
+    // numbers.unshift(120);
+    // // Take of from end
+    // numbers.pop();
+    // // Take of from front
+    // numbers.shift();
+    // // Splice values
+    // numbers.splice(1,3);
+    // // Reverse array
+    // numbers.reverse();
+
+    // Concatenate array
+    val = numbers.concat(numbers2);
+
+    // // Sorting arrays
+    // val = fruit.sort();
+    // val = numbers.sort();
+
+    // // Use the "compare function" to sort numbers
+    // val = numbers.sort(function(x, y){return x-y;});
+
+    // // Reverse sort using "compare function" for numbers
+    // val = numbers.sort(function(x, y){return y-x;});
+
+    // Find - in example you can also use <
+    function over50(num){
+        return num > 50;
+    }
+
+    val = numbers.find(over50)
+
+
+
+    console.log(numbers);
+    console.log(val);
+*/
