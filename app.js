63be4c6f4fbb772fae471177dbefe89376029d8d
Jordy1311 <jordancooper1195@gmail.com>
2021-03-11 14:43:10 +1300

    Object Literals

--- app.js
+++ app.js
@@ -10,8 +10,10 @@
     console.log('Hello World');
     console.log('123');
     console.log('true');
+
     var greeting = 'Hello';
     console.log(greeting);
+
     console.log([1,2,3,4]);
     console.log({a:1, b:2, c:3});
     console.table({a:1, b:2, c:3});
@@ -216,8 +218,49 @@ console.log(typeof today);
 
     val = numbers.find(over50)
 
-
-
     console.log(numbers);
     console.log(val);
+*/
+
+/*
+    // OBJECT LITERALS
+    const person = {
+        firstName: 'Steve',
+        lastName: 'Smith',
+        age: 36,
+        email: 'steve@aol.com',
+        hobbies: ['music', 'sports'],
+        address: {
+            city: 'Miami',
+            state: 'Florida'
+        },
+        getBirthYear: function(){
+            return 2017 - this.age;
+        }
+    }
+
+    let val;
+
+    val = person;
+    // Get specific value - first is reccomended
+    val = person.firstName;
+    val = person['lastName'];
+
+    val = person.age;
+    val = person.hobbies[1];
+    val = person.address.state;
+    val = person.getBirthYear();
+
+
+    console.log(val);
+
+    const people = [
+        {name: 'John', age: 30},
+        {name: 'Mike', age: 23},
+        {name: 'Nancy', age: 40}
+    ];
+
+    for(let i = 0; i < people.length; i++){
+        console.log(people[i].name)
+    }
 */
