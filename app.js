217a4268c42c5e841d31f36d3595e6342ed2d6c2
Jordy1311 <jordancooper1195@gmail.com>
2021-03-16 10:26:35 +1300

    Function Declarations & Expressions

--- app.js
+++ app.js
@@ -535,7 +535,7 @@
 */
 
 
-// SWITCHES
+/* SWITCHES
     const color = 'yellow';
 
     switch(color){
@@ -577,3 +577,52 @@
     }
 
     console.log(`Today is ${day}`);
+*/
+
+
+// FUNCTION DECLARATIONS & EXPRESSIONS
+    // Declarations
+    function greet(firstName = 'John', lastName = 'Doe'){ //String here (is default) accomplishes as 2 if statements below
+        // if(typeof firstName === 'undefined'){firstName = 'John'}
+        // if(typeof lastName === 'undefined'){lastName = 'Doe'}
+
+        //console.log('Hello');
+
+        return 'Hello ' + firstName + ' ' + lastName;
+    }
+
+    // console.log(greet());
+
+    // Function Expressions
+    const square = function(x = 3){ // 3 here is default
+        return x*x;
+    };
+
+    // console.log(square(8));
+
+    // Immidiatley Invokable Function Expressions - IIFEs
+    // (function(){
+    //     console.log('IFFEE Ran..')
+    // })();
+
+    // (function(name){
+    //     console.log('Hello ' + name)
+    // })('Brad');
+
+    // Property Methods
+    const todo = {
+        add: function(){
+            console.log('Add todo..');
+        },
+        edit: function(id){
+            console.log(`Edit todo ${id}`);
+        }
+    }
+
+    todo.delete = function(){
+        console.log('Delete todo...');
+    }
+
+    todo.add();
+    todo.edit(22);
+    todo.delete();
