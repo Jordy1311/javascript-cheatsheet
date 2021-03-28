91ead5b1fd9224ae055746a9a2be26a966cc160d
Jordy1311 <jordancooper1195@gmail.com>
2021-03-29 11:52:38 +1300

    DOM Selectors For Single Elements

--- DOM_manipulation_&_events/app.js
+++ DOM_manipulation_&_events/app.js
@@ -1,4 +1,4 @@
-/* Examining The Document Object
+/* EXAMINING THE DOCUMENT OBJECT
   let val;
 
   val = document;
@@ -41,3 +41,41 @@
   console.log(val);
 */
 
+/* DOM SELECTORS FOR SINGLE ELEMENTS
+  // document.getElementById()
+  //   console.log(document.getElementById('task-title'));
+
+  // // Get things from the element
+  //   console.log(document.getElementById('task-title').id);
+  //   console.log(document.getElementById('task-title').className);
+
+  // const taskTitle = document.getElementById('task-title');
+  
+  // // Change styling
+  //   taskTitle.style.background = '#333';
+  //   taskTitle.style.color = '#fff';
+  //   taskTitle.style.padding = '5px';
+  //   // taskTitle.style.display = 'none';
+
+  // // Change content
+  //   taskTitle.textContent = 'Task List';
+  //   taskTitle.innerText = 'My Tasks';
+  //   taskTitle.innerHTML = '<span style="color:red">Task List</span>'; // Insert HTML
+
+  // document.querySelector()
+
+    console.log(document.querySelector('#task-title'));
+    console.log(document.querySelector('.card-title'));
+    console.log(document.querySelector('h5')); // Will only select first element as specified
+
+    // Will only make changes to first element as specified
+      document.querySelector('li').style.color = 'red';
+      document.querySelector('ul li').style.color = 'blue';
+
+    // To select last use css sudo classes
+    document.querySelector('li:last-child').style.color = 'red';
+    document.querySelector('li:nth-child(3)').style.color = 'yellow';
+    document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+    document.querySelector('li:nth-child(odd)').style.background = '#ccc'; // will only select first "odd"
+    document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';  // will only select first "even"
+*/
