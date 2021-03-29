372ad63dfc332fdd054bb9f1a23444f81dfc1c2b
Jordy1311 <jordancooper1195@gmail.com>
2021-03-30 11:16:54 +1300

    Creating Elements using Javascript

--- DOM_manipulation_&_events/app.js
+++ DOM_manipulation_&_events/app.js
@@ -197,4 +197,37 @@
   // You can link together to move around
 
   console.log(val);
-  */
+*/
+
+
+/* CREATING ELEMENTS
+  // Create Element
+    const li = document.createElement('li');
+
+  // Add class
+    li.className = 'collection-item';
+
+  // Add id
+    li.id = 'new-item';
+
+  // Add attribute
+    li.setAttribute('title', 'New Item');
+
+  
+  // Create text node and append
+    li.appendChild(document.createTextNode('List Item - Created with Javascript'));
+
+  // Create new link element
+    const link = document.createElement('a');
+    // Add classes
+      link.className = 'delete-item secondary-content';
+    // Add icon html within a tag
+      link.innerHTML = '<i class="fa fa-remove"></i>'
+    // Append link into li
+      li.appendChild(link);
+
+  // Append li as child to ul
+    document.querySelector('ul.collection').appendChild(li);
+
+  console.log(li);
+*/
