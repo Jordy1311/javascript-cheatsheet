ead34adcaba1293149f00de185b069877a3abca5
Jordy1311 <jordancooper1195@gmail.com>
2021-03-29 14:00:56 +1300

    DOM Selectors For Multiple Elements

--- DOM_manipulation_&_events/app.js
+++ DOM_manipulation_&_events/app.js
@@ -79,3 +79,56 @@
     document.querySelector('li:nth-child(odd)').style.background = '#ccc'; // will only select first "odd"
     document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';  // will only select first "even"
 */
+
+/* DOM SELECTORS FOR MULTIPLE ELEMENTS
+  // document.getElementsByClassName  
+    // const items = document.getElementsByClassName('collection-item');
+    // console.log(items);
+
+    // console.log(items[0]);
+    // items[0].style.color = 'red';
+    // items[3].textContent = 'Hello World';
+
+    // const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+    // console.log(listItems); // Produces same as above but if class name was outside of querySelector then it will not show
+
+  // document.getElementsByTagName
+    //   let lis = document.getElementsByTagName('li');
+    //   console.log(lis);
+
+    //   console.log(lis[0]);
+    //   lis[0].style.color = 'red';
+    //   lis[3].textContent = 'Hello World';
+
+    // // Convert HTML Collection into Array
+    // lis = Array.from(lis);
+
+    // lis.reverse();
+
+    // lis.forEach(function(li, index){
+    //   console.log(li.className);
+    //   li.textContent = `${index}: Hello`;
+    // });
+
+    // console.log(lis);
+
+  // document.querySelectorAll
+    const items = document.querySelectorAll('ul.collection li.collection-item');
+
+    items.forEach(function(item, index){
+      item.textContent = `${index}: Hello`;
+    });
+
+    const liOdd = document.querySelectorAll('li:nth-child(odd)');
+    const liEven = document.querySelectorAll('li:nth-child(even)');
+
+    liOdd.forEach(function(li, index){
+      li.style.background = '#ccc';
+    });
+
+    for(let i = 0; i < liEven.length; i++){
+      liEven[i].style.background = 'f4f4f4';
+    }
+
+    console.log(items);
+*/
