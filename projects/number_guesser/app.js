b3e30f5fd82b052a0e0fdf94016447db4039d9ec
Jordy1311 <jordancooper1195@gmail.com>
2021-04-08 09:19:15 +1200

    Number Guesser - Lose Case & Game Over

--- projects/number_guesser/app.js
+++ projects/number_guesser/app.js
@@ -37,23 +37,47 @@ guessBtn.addEventListener('click', function() {
     if(isNaN(guess)) {
         setMessage(`You can't guess nothing!`, 'red');
     } else if (guess < min) {
-        setMessage(`Your number was smaller than ${min}!  ¯\\_(ツ)_/¯`, 'red');
+        setMessage(`Your number was smaller than ${min}  ¯\\_(ツ)_/¯`, 'red');
     } else if (guess > max) {
         setMessage(`Your number was higher than ${max}  ¯\\_(ツ)_/¯`, 'red');
-    }
+    } 
 
     // Check if won
     if(guess === winningNum) {
-        // Disable input
-        guessInput.disabled = true;
-        // Change border color
-        guessInput.style.borderColor = 'green';
-        // setMessage win
-        setMessage(`${winningNum} is correct! You win!!`, 'green');
+        // GAME OVER - WON
+        gameOver(true, `${winningNum} is correct! You won!!`);
     } else {
-
+        // Wrong number
+        guessesLeft -=1;
+        // Check guesses left
+        if(guessesLeft === 0) {
+            // GAME OVER - LOST
+            gameOver(false, `Game over, you lost! The correct number was ${winningNum}.`);
+            } else {
+            // Game continues - answer wrong
+            // Change border color
+            guessInput.style.borderColor = 'red';
+            // Tell user, wrong guess
+            setMessage(`${guess} is not correct. ${guessesLeft} guesses left!`, 'red');
+            // Clear input
+            guessInput.value = '';
+        }
     }
 });
+
+// gameOver();
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable input
+    guessInput.disabled = true;
+    // Change border & text color
+    guessInput.style.borderColor = color;
+    message.style.color = color;
+    // setMessage win
+    setMessage(msg);
+}
 
 // setMessage();
 function setMessage(msg, color) {
