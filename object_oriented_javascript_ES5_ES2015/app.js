8c6c8e50c9cd2b498c7d5cbc363bf8cc2547298e
Jordy1311 <jordancooper1195@gmail.com>
2021-04-09 11:10:00 +1200

    Built in Constructors

--- object_oriented_javascript_ES5_ES2015/app.js
+++ object_oriented_javascript_ES5_ES2015/app.js
@@ -40,3 +40,63 @@
 */
 
 
+/*
+// BUILT IN CONSTRUCTORS
+// There are other core objects that have constructors but these are not advisable to use
+// Primative data types can be created as objects using constructors
+// There is not many reasons to do this and reasons not to
+// Slower code, issues using === comparing value and type
+
+// Strings
+  const name1 = 'Jeff'
+  const name2 = new String('Jeff');
+
+  // console.log(name1) // gives you the value
+  // console.log(name2) // gives you an object with value inside
+    // You can add properties to name2 but there isnt many reasons to do this
+
+    console.log(typeof name1) // returns string
+    console.log(typeof name2) // returns object
+
+  // Issues arrise when
+  if(name1 === 'Jeff'){
+    console.log("name1 === 'Jeff'")
+  } else {
+    console.log("name1 !== 'Jeff'")
+  }
+
+  if(name2 === 'Jeff'){
+    console.log("name2 === 'Jeff'")
+  } else {
+    console.log("name2 !== 'Jeff'")
+  }
+  // if the name2's if statement checked name2 against == rather than === it would be a true statement
+
+
+// Other examples
+  const num1 = 5
+  const num2 = new Number(5)
+
+  const bool1 = true
+  const bool2 = new Boolean(true)
+
+  const getSum1 = function(x, y) {
+    return x + y
+  }
+  const getSum2 = new Function('x', 'y', 'return x + y')
+
+    console.log(getSum2(1,3))
+
+  // Object and Array example - no difference with output as already objects, just longer syntax
+  const john1 = {name: 'John'}
+  const john2 = new Object({name: 'John'})
+
+  const arr1 = [1,2,3,4]
+  const arr2 = new Array(5,6,7,8)
+
+  // Regular Expressions - will be confusing if you have not covered REs yet
+  const re1 = /\w+/ // this RE is looking for a word character that occures 1 or more times
+  const re2 = new RegExp ('\\w+') // when using RE like this you need to escape the backslash
+
+  console.log(re2)
+  */
