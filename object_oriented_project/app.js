6b06f8c228c845a974317479da19afe391437a6e
Jordy1311 <jordancooper1195@gmail.com>
2021-04-12 17:53:02 +1200

    Book List - Validation & Alert

--- object_oriented_project/app.js
+++ object_oriented_project/app.js
@@ -24,6 +24,25 @@ UI.prototype.addBookToList = function(book) {
     list.appendChild(row)
 }
 
+// show alert
+UI.prototype.showAlert = function(message, className) {
+    // create div
+    const div = document.createElement('div')
+    // add classes
+    div.className = `alert ${className}`
+    // add text
+    div.appendChild(document.createTextNode(message))
+    // get parent
+    const container = document.querySelector('.container')
+    const form = document.querySelector('#book-form')
+    // insert alert
+    container.insertBefore(div, form)
+    // timeout after 3 sec
+    setTimeout(function() {
+        document.querySelector('.alert').remove()
+    }, 3000)
+}
+
 // clear fields
 UI.prototype.clearFields = function() {
     document.getElementById('title').value = ''
@@ -45,11 +64,19 @@ document.getElementById('book-form').addEventListener('submit',
         // instantiate UI
         const ui = new UI()
 
-        // add book to list
-        ui.addBookToList(book)
+        // validate
+        if(title === '' || author === '' || isbn === '') {
+            ui.showAlert('Stop!! Please fill in all fields.', 'error')
+        } else {
+            // add book to list
+            ui.addBookToList(book)
 
-        // clear fields
-        ui.clearFields()
+            // show success
+            ui.showAlert('Book added!!', 'success')
+
+            // clear fields
+            ui.clearFields()
+        }
 
         e.preventDefault()
     })
