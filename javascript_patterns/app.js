dfe8bb992d4605aa423296ca73c6537da3edc2bc
Jordy1311 <jordancooper1195@gmail.com>
2021-04-22 08:43:09 +1200

    Factory Pattern

--- javascript_patterns/app.js
+++ javascript_patterns/app.js
@@ -105,3 +105,60 @@ console.log(instanceB);
 console.log(instanceA === instanceB);
 // you can have more than one instance from the Singleton but they will be the same as above
 */
+
+
+/*
+// FACTORY PATTERN
+// way of creating an interface that creates objects but let subclasses define which classes to instantiate
+// member - paid member (different types but still have the same methods)
+
+function MemberFactory() {
+    this.createMember = function(name, type) {
+        let member;
+
+        if(type === 'simple') {
+            member = new SimpleMembership(name);
+        } else if(type === 'standard') {
+            member = new StandardMembership(name);
+        } else if(type === 'super') {
+            member = new SuperMembership(name);
+        }
+
+        member.type = type;
+
+        member.define = function() {
+            console.log(`${this.name} (${this.type}): ${this.cost}`);
+        }
+
+        return member;
+    }
+}
+
+const SimpleMembership = function(name) {
+    this.name = name;
+    this.cost = '$5';
+}
+
+const StandardMembership = function(name) {
+    this.name = name;
+    this.cost = '$15';
+}
+
+const SuperMembership = function(name) {
+    this.name = name;
+    this.cost = '$25';
+}
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember('John Doe', 'simple'));
+members.push(factory.createMember('Chris Jackson', 'super'));
+members.push(factory.createMember('Janice Williams', 'simple'));
+members.push(factory.createMember('Tom Smith', 'standard'));
+
+// console.log(members)
+members.forEach(function(member) {
+    member.define();
+});
+*/
