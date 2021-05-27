22934e635dfb04f8bd6969f541a407eb40cb418c
Jordy1311 <jordancooper1195@gmail.com>
2021-05-27 21:55:18 +1200

    Custom HTTP Library (Ajax With Callbacks) - Part 2

--- asynchronous_javascript_ajax_fetch_API/custom_HTTP_library_(Ajax_With_Callbacks)_Part_1/app.js
+++ asynchronous_javascript_ajax_fetch_API/custom_HTTP_library_(Ajax_With_Callbacks)_Part_1/app.js
@@ -1,11 +1,54 @@
 const http = new easyHTTP;
 
-// get posts
-// this has been done asyncronusly with a callback
-http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts) {
+// // get posts
+// // this has been done asyncronusly with a callback
+// http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// // get single post
+// http.get("https://jsonplaceholder.typicode.com/posts/1", function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+
+// create data
+const data = {
+  title: "Custom Post",
+  body: "This is a custom post"
+};
+
+// // create post - note that id is 101 which was made by the rest api
+// http.post("https://jsonplaceholder.typicode.com/posts/", data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// // update post - note that we have amended the post with the id: 1
+// http.put("https://jsonplaceholder.typicode.com/posts/1", data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// })
+
+// delete post
+http.delete("https://jsonplaceholder.typicode.com/posts/1", function(err, response) {
   if(err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(response);
   }
 });
--- asynchronous_javascript_ajax_fetch_API/custom_HTTP_library_(Ajax_With_Callbacks)_Part_1/easyhttp.js
+++ asynchronous_javascript_ajax_fetch_API/custom_HTTP_library_(Ajax_With_Callbacks)_Part_1/easyhttp.js
@@ -20,5 +20,43 @@ easyHTTP.prototype.get = function(url, callback) {
 }
 
 // make an HTTP POST request
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open("POST", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+
+  let self = this;
+  this.http.onload = function() {
+      callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
 // make an HTTP PUT request
+easyHTTP.prototype.put = function(url, data, callback) {
+  this.http.open("PUT", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+
+  let self = this;
+  this.http.onload = function() {
+      callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
 // make an HTTP DELETE request
+easyHTTP.prototype.delete = function(url, callback) {
+  this.http.open("DELETE", url, true);
+
+  let self = this;
+  this.http.onload = function() {
+    if(self.http.status === 200) {
+      callback(null, "Post deleted!!");
+    } else {
+      callback("Error: " + self.http.status);
+    }
+  };
+
+  this.http.send();
+}
